34dfd75fea853144339141e9403aa0710d3508bd
Kur1su0 <1997weizice@gmail.com>
2019-02-03 01:04:22 -0500

    program1 done

--- proj1/leakcount.c
+++ proj1/leakcount.c
@@ -10,14 +10,17 @@
 
 int main(int argc, char **argv)
 {
+    
     //read to fork.
     if(!fork()){ //child
 		execvp(argv[1],&argv[1]);
+
 	}else{ 
 		//parent
-		wait(NULL);   
+        wait(NULL);  
 		//count for leaks and frees
 	}
+    abort();
 
 	return 0;
 
--- proj1/run.sh
+++ proj1/run.sh
@@ -1,2 +1,3 @@
 #$1
+make
 LD_PRELOAD=./memory_shim.so ./leakcount $1
