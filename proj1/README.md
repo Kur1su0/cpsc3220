155b54364c900c7b4b3e970ff31578f8ac39e2f9
Kur1su0 <1997weizice@gmail.com>
2019-02-05 13:37:31 -0500

    prog2 finished

--- proj1/README.md
+++ proj1/README.md
@@ -2,10 +2,14 @@ Zice Wei
 
 ## Project 1.
 ### KNOWN PROBLEMS: 
-                1. Just implement the program 1.
-                2. command such as ls, cat, cp doesn't work.
-                   (child process not even running for these cases).
-                3.TODO program 2 : PTRACE
+                1.program 2 : additional exit_group(231).
+                              missed execve.
 ### DESIGN:
-                1. designed with link list.
-                2. fork+execvp.
+                prog 1  1. designed with link list.
+                        2. fork+execvp.
+                
+                prog 2  1. designed wirh array.
+                        2. fork+execvp.
+                        
+                        reference: 1.(64-bit syscall table)[http://blog.rchapman.org/posts/Linux_System_Call_Table_for_x86_64/]
+                                   2.(sample code for ptrace)[https://stackoverflow.com/questions/11081859/how-to-trace-a-process-for-system-calls]
--- proj1/sctracer.c
+++ proj1/sctracer.c
@@ -1,15 +1,113 @@
 #include <stdio.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/ptrace.h>
+#include <fcntl.h>
 #include <string.h>
+#include <sys/user.h>
+#include <sys/reg.h>
+#include <signal.h>
+
+#define MAXSIZE 1000
 
 
+typedef struct trace_info{
+    int sys_num;
+    int time;
+    struct trace_info* next;   
+}info;
+
+info* HEAD = NULL;
+info* TAIL = NULL;
+
+void write_to_file(long system_call_table[MAXSIZE], char* output_file);
+
+//int* add_syscall_info(long* info[MAXSIZE], long id){
+//    add_syscall_info[(int)id]+=1;
+//    return info;
+//}
 int main(int argc, char **argv) {
     //TODO : ptrace;
+    
+    /* Split command in " ".  */
+    char*ptr = strtok(argv[1]," ");
+    char* cmd[MAXSIZE] ={NULL};
+    char* output_file = argv[2];
+    int len = 0;
+    long current_call;
+    long last_call;
+    int status = 0;
+    long system_call_table[MAXSIZE]= {(long)0};
+    pid_t child;
+    while(ptr!=NULL){
+        cmd[len++] = ptr; 
+        ptr = strtok(NULL," ");
+    }
+    cmd[len] = NULL;
+    len++;
+    
+    child = fork();  
+    if(child==0){
+        ptrace(PTRACE_TRACEME,getpid(),NULL,NULL);
+        execvp(cmd[0],cmd);
+        //kill(getpid(),SIGKILL);
+        
+    //TODO: call ptrace;
+    //      ptrace(PTRACE_TRACEME);
+    //      ptrace(PTRACE_SYSCALL);
+    //      ptrace(PTRACE_PEEKUSER);
+    //      ptrace(PTRACE_SETOPTIONS);
+    //      wait pid
+    //      getpid.
+    //execvp(cmd[0],cmd);
 
+    }
+    else{
+        //Distinguish between sigtop and other system call.
+        //ptrace(PTRACE_SETOPTIONS,child,0,PTRACE_O_TRACESYSGOOD);
+        ptrace(PTRACE_SETOPTIONS,child,0,PTRACE_O_TRACEEXIT);
+        while(1){
+            //wait(&status);
+            waitpid(child,&status,0);
+            if(WIFEXITED(status) || WIFSIGNALED(status)){
+                break;
+            }
+            if(WIFSTOPPED(status) && WSTOPSIG(status)==SIGTRAP){
+                current_call = ptrace(PTRACE_PEEKUSER, child,sizeof(long)*ORIG_RAX,NULL);
+                system_call_table[(int)current_call]++;
+            }
+            ptrace(PTRACE_SYSCALL,child,0,0);
+        }
 
+    }
+    last_call = current_call;
+    for(int i=0;i < MAXSIZE;i++){ 
+        if(system_call_table[i]!=0 && i!=last_call){
+            system_call_table[i]/=(long)2;
+        }
+    }
+    write_to_file(system_call_table,output_file);
+    
+
+    return 0;
 }
+
+void write_to_file(long table[MAXSIZE], char* file_path){
+    int File;
+    File = open(file_path,O_CREAT|O_RDWR,0666); //Not a Magic number.
+    if(File<0) perror("err open file");
+    char info[MAXSIZE];
+    for(int i=0;i < MAXSIZE;i++){ 
+        if(table[i]!=0){
+            sprintf(info,"%d\t%ld\n",i,table[i]);
+            write(File,info,strlen(info));
+        }
+    }
+
+    close(File);
+}
+
+
