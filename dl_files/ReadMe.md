7932ad2b874daa5db6dd308f8b1fe2b575303138
Kur1su0 <1997weizice@gmail.com>
2019-05-15 11:03:00 -0400

    add dl_script

---
+++ dl_files/ReadMe.md
@@ -0,0 +1,4 @@
+#h4 Usage
+1. place *.py & *.sh in the same dir. 
+2. sh run.sh
+
---
+++ dl_files/run.sh
@@ -0,0 +1,25 @@
+if [ -d "file/" ];then
+    echo "file/ exits"
+else
+    echo "file/ not exits"
+    echo "dir file/ created "
+    mkdir file
+fi
+
+
+#lib req for python: requests. beautifulSoup
+echo "DL documents"
+python dl.py
+#uncomment below to convert pptx to pdf
+echo "convert pptx to pdf"
+if [ -d "slides/" ];then
+    echo "slides/ exits"
+else
+    echo "slides/ not exits"
+    mkdir slides
+    echo "dir slides/ created "
+fi
+cp file/*.pptx slides/
+cd slides
+libreoffice --headless --invisible --convert-to pdf *.pptx
+rm -v *.pptx
