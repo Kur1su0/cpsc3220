9ed8fc2bf00e0c3a7a42df2192fb145947a5271a
Zice Wei <zicew@ada1.computing.clemson.edu>
2019-06-14 13:14:01 -0400

    stdin

--- new/proj4/paging.c
+++ new/proj4/paging.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int verbose = 0;
@@ -21,11 +22,50 @@ typedef struct{
     unsigned short vpn;       /* 16 bits */
   } CME;
 
+  typedef struct{
+      int PF;
+      int TE;
+      int UP;
+  } info;
 
-PTE _PTE;
-TLBE _TLBE;
-CME _CME;
+PTE pte;
+TLBE tlbe;
+CME cme;
+info cfg;
+char buf [100][7];
+int len = 0;
 
+void readData(){
+    FILE* fptr = NULL;
+    if( (fptr=fopen("paging.cfg","rb"))==NULL ){
+         printf("bad file path\n");
+         exit(1);
+    }
+    char junk[100];
+    fscanf(fptr,"%s %d\n",junk,&cfg.PF);
+    fscanf(fptr,"%s %d\n",junk,&cfg.TE);
+    fscanf(fptr,"%s %d\n",junk,&cfg.UP);
+    fclose(fptr);
+}
+void readBuf(){
+    int i=0;
+    while (!feof(stdin)){
+       fscanf(stdin,"%s\n",buf[i]);
+       i++;
+    }
+    len = i+1;
+}
+void printData(){
+   printf("PF:%d\n",cfg.PF);
+   printf("TE:%d\n",cfg.TE);
+   printf("UP:%d\n",cfg.UP);
+   int i=0;
+   for(i=0;i<100;i++) {
+        if(i+1==len) break;
+        printf("%s\n",buf[i]);
+    }
+
+}
 
 int main(int argc, char* argv[]){
     if(argc == 2){
@@ -33,7 +73,11 @@ int main(int argc, char* argv[]){
     }
     printf("verbose:%d\n",verbose);
     //Read input.
+    readData();
+    readBuf();
+    printData();
     
+    //stdin stream
     return 0;
 
 
